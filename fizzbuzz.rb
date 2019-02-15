9a9f79f9f14d6cbaa27fd46c4c188efb1c9a2aee
placid-computer-9089 <zubairmaqsood866@gmail.com>
2019-02-15 21:12:13 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,13 +4,11 @@
 def fizzbuzz(number)
   if number % 3 == 0 
     return "Fizz"
-  end
   
-  if number % 5 == 0 
+  else number % 5 == 0 
     return "Buzz"
-  end
   
-  if number % 3 == 0 && number % 5 == 0 
+  elsif number % 3 == 0 && number % 5 == 0 
     return "FizzBuzz"
   end 
 
